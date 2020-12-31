23dba02e15884fe04efa1171b52a815c51554226
景山陽介 <kageyamayousuke@mbp.local>
2020-12-31 12:43:27 +0900

    leran props of component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 function App() {
-  return <h1 className="foo">hello</h1>;
+  const profiles = [
+    { name: "Taro", age:10 },
+    { name: "Hanako", age:5 },
+    { name: "Noname"}
+  ]
+  return (
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
+    </div>
+  )
 }
 
+const User = (props) => {
+  return <div>Hi, I am {props.name}, and { props.age} years old!</div>
+}
+
+User.defaultProps = {
+  age: 1
+}
 export default App;
